c7417fc115e1726ca5f6946581dd181514ccdd78
Uttah <au.fedot@gmail.com>
2018-04-10 14:45:09 +0300

    Введена обработка погрешности

--- optimization/cas.py
+++ optimization/cas.py
@@ -1,3 +1,5 @@
+import math
+
 def fx(x):
     return x*x*x + 8*x*x + x +5
 
@@ -7,10 +9,15 @@ def fx1(x):
 
 def cas(iter):
     x1 = float(input("Введите x1 - "))
+    eps = float(input("Введите погрешность - "))
     for i in range(iter):
         xn = x1 - (fx(x1)/fx1(x1))
+        if (math.fabs(xn - x1) < eps) or \
+            (math.fabs(fx1(xn))) < eps:
+            print("Погрешность превышена!!!")
+            break
         x1 = xn
-        print("x = ", xn)
+        print("Итерация №{}: x = {}, y = {}".format(i+1, xn, fx(xn)))
 
 kolvo = int(input("Укажите количество итераций - "))
 cas(kolvo)
